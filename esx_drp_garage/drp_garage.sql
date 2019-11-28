41f6fd9a3e33bbd440dcafab87eade603a6691b5
Morow73 <39746007+Morow73@users.noreply.github.com>
2019-11-28 15:39:22 +0100

    Update drp_garage.sql

--- esx_drp_garage/drp_garage.sql
+++ esx_drp_garage/drp_garage.sql
@@ -1,4 +1,3 @@
 ALTER TABLE `owned_vehicles` ADD `state` BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'Etat de la voiture' AFTER `owner`;
 
-ALTER TABLE `owned_vehicles` ADD `garage_position` BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'Garage Pos' AFTER `state` ;
 
