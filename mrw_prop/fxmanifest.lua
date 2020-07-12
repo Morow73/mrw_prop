6f8445a07bbd5fb2506b8a7607db6c66867ceb70
Morow <morowind73@gmail.com>
2020-07-12 11:42:11 +0200

    fix keyboard input + add warmenu in resource

--- mrw_prop/fxmanifest.lua
+++ mrw_prop/fxmanifest.lua
@@ -4,16 +4,13 @@ description 'Mrw_Prop'
 
 author 'Morow'
 
-client_script '@warmenu/warmenu.lua'
-
 client_scripts {
-
-  'client.lua'
+  'client/warmenu.lua',
+  'client/client.lua'
 }
 
 
 server_scripts {
-
   '@mysql-async/lib/MySQL.lua',
-  'server.lua'
+  'server/server.lua'
 }
